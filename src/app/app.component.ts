22e22ce5e5f65ab695a1678c13e9f4750a0eac4a
aravind <aravindcbk@gmail.com>
2025-09-13 18:54:49 +0530

    Updating local

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Clan } from './clan';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'CRUD-UI';
+  clans: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  getClans() {
+    this.http.get<Clan[]>('http://localhost:8080/getclans').subscribe(data => {
+      console.log(data); // Add this line
+      this.clans = data;
+    });
+  }
 }
---
+++ src/app/clan.ts
@@ -0,0 +1,4 @@
+export interface Clan {
+    id: number;
+  name: string;
+}
